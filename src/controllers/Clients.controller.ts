8fc07a5f434276de25b3e68b0b03bd48dd274af9
rafaleceu <rafa021dev@gmail.com>
2021-03-12 17:08:57 -0300

    feat: create client when create user

--- src/controllers/Clients.controller.ts
+++ src/controllers/Clients.controller.ts
@@ -3,20 +3,22 @@ import { getRepository } from 'typeorm';
 
 import { Clients } from '../models/Clients.model';
 
+
 class ClientsController {
 
-    public  create = async (idUser: number, name: string, adress: string, phone: string): Promise<boolean> => {
+    public create = async (idUser: number, name: string, adress: string, phone: string): Promise<boolean> => {
 
         const repository = getRepository(Clients);
 
         try {
             await repository.save({idUser, name, adress, phone});
         } catch (err) {
-            return false
+            throw err
         }
+
         return true
     };
 
 };
 
-export default new ClientsController;
+export default new ClientsController();
--- src/controllers/Users.controller.ts
+++ src/controllers/Users.controller.ts
@@ -1,22 +1,25 @@
 import { RequestHandler, Request, Response, NextFunction } from 'express';
 import { getRepository } from 'typeorm';
 
+import ClientsController from './Clients.controller';
+import AccountsController from './Accounts.controller';
 import { Users } from '../models/Users.model';
-
 class UsersController {
 
     public  create: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
 
         const repository = getRepository(Users);
 
-        let { username, password, email, name, cpf } = req.body;
-        let query;
+        let { username, password, email, name, cpf, adress, phone } = req.body;
 
-        if (!username || !password || !email || !name || !cpf) 
+        if (!username || !password || !email || !name || !cpf || !adress || !phone) 
             return res.status(400).send('Estão faltando campos');
         
+        let query;
+        
         try {
-            query = await repository.save({username, password, email, name, cpf});
+            query = await repository.save({username, password, email, cpf});
+            await ClientsController.create(query.id, name, adress, phone);
         } catch (err) {
             return res.status(400).json(err.message);
         };

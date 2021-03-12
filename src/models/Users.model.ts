1f403d651c41300808e893d9067d41ff5faf15cb
rafaleceu <rafa021dev@gmail.com>
2021-03-12 09:36:54 -0300

    feat: user model

---
+++ src/models/Users.model.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+ 
+@Entity() 
+export class Users {
+
+    @PrimaryGeneratedColumn({ unsigned: true, type: 'int' })
+    id!: number;
+
+    @Column({ type: 'varchar', length: '255', unique: true })
+    username!: string
+
+    @Column({ type: 'varchar', length: '255' })
+    password!: string
+
+    @Column({ type: 'varchar', length: '255', unique: true })
+    email!: string
+
+    @Column({ type: 'varchar', length: '50' })
+    name!: string
+
+    @Column({ type: 'char', length: '11', unique: true })
+    cpf!: string
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+    createdAt!: Date
+
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+    updatedAt!: Date
+    
+}

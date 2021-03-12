eae1e8008c337b58c1e77fb9c73f7e2488ecdb97
rafaleceu <rafa021dev@gmail.com>
2021-03-12 09:34:14 -0300

    feat: add .env.example & add configs

---
+++ src/config/configs.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+export default {
+    Server: {
+        port: process.env.SERVER_PORT
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import Express from './server/Express';
+import configs from './config/configs';
 
-Express.server.listen(8080, () => console.log(`Servidor rodando na porta 8080`));
+Express.express.listen(configs.Server.port, () => {
+    console.log(`Servidor rodando na porta ${configs.Server.port}`)
+})

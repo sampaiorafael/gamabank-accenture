f817ceb00cbe5896f165f255758d4d01862f9dad
rafaleceu <rafa021dev@gmail.com>
2021-03-12 09:35:59 -0300

    feat: connect to mysql & express use body-parser

--- src/server/Express.ts
+++ src/server/Express.ts
@@ -1,19 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import { createConnection } from 'typeorm';
 
 import routes from '../routes/routes';
 
 class Express {
 
-    public server: any
+    public express;
 
     constructor() {
-        this.server = express();
+        this.express = express();
+        this.middlewares();
         this.routes();
+        this.database();
     }
 
+    private middlewares() {
+        this.express.use(bodyParser.urlencoded({extended: true}));
+   }
+
    private routes() {
-       this.server.use(routes);
-   } 
+        this.express.use(routes);
+   }
+
+   private async database(){
+        return await createConnection();
+   }
 
 }
 

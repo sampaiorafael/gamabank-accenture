3bf6b509bb920f0dff96d54adc586daef9c7ce08
yahnnThomas <yahnnis@gmail.com>
2021-03-15 13:00:18 -0300

    refac: correção nome variável axperted - expected

--- src/test/validateCpf.spec.ts
+++ src/test/validateCpf.spec.ts
@@ -5,23 +5,23 @@ const assert = chai.assert
 
 describe('Teste para validação de CPF', () => {
     it ('Deve remover os caracteres que não digitos', () => {
-        const expertedResult = true
-        assert.equal( validateCpf('073.785.344-12'), expertedResult )
+        const expectedResult = true
+        assert.equal( validateCpf('073.785.344-12'), expectedResult )
     })
 
     it ('Deve retorar false para tamanho de CPF diferente de 11', () => {
-        const expertedResult = false
-        assert.equal( validateCpf('0731234561'), expertedResult )
+        const expectedResult = false
+        assert.equal( validateCpf('0731234561'), expectedResult )
     })
 
     it ('Deve retornar false em caso falha na validação do digito verificador', () => {
-        const expertedResult = false
-        assert.equal( validateCpf('07378534411'), expertedResult )
+        const expectedResult = false
+        assert.equal( validateCpf('07378534411'), expectedResult )
     })
 
     it ('Deve retornar false em caso de todos os numeros iguais', () => {
-        const expertedResult = false
-        assert.equal( validateCpf('11111111111'), expertedResult )
+        const expectedResult = false
+        assert.equal( validateCpf('11111111111'), expectedResult )
     })
 
 })

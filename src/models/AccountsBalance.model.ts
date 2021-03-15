147eb8fc4294160220088661f24102747e78aeb5
rafaleceu <rafa021dev@gmail.com>
2021-03-15 12:39:57 -0300

    refac: AccountBalance & Movemente models

--- src/models/AccountsBalance.model.ts
+++ src/models/AccountsBalance.model.ts
@@ -1,24 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
  
-@Entity({name: 'accounts_balance'}) 
+@Entity() 
 export class AccountsBalance {
 
     @PrimaryGeneratedColumn({ unsigned: true, type: 'int' })
     id!: number;
 
     @Column({ name: 'account_id', type: 'int' })
-    acountId!: number
+    accountId!: number
 
-    @Column({ name: 'month_year', type: 'varchar', length: '45' })
-    monthYear!: string
+    @Column({ type: 'int' })
+    month!: number
 
-    @Column({ name: 'inicital_balance', type: 'decimal' })  
+    @Column({ name: 'initial_balance', type: 'decimal' })  
     initialBalance!: number
 
-    @Column({ type: 'decimal' }) 
+    @Column({ name: 'actual_balance', type: 'decimal' }) 
     actualBalance!: number
 
-    @Column({ type: 'decimal' }) 
+    @Column({ name: 'final_balance', type: 'decimal' }) 
     finalBalance!: number
 
     @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
--- src/models/AccountsMovement.model.ts
+++ src/models/AccountsMovement.model.ts
@@ -1,30 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Timestamp } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from 'typeorm';
 
 @Entity() 
-export class CreditCardMovment {
+export class AccountsMovement {
 
     @PrimaryGeneratedColumn({ unsigned: true, type: 'int' })
     id!: number;
 
-    @Column({ name: 'id_credit_card', type: 'int' })
-    idCreditCard!: number
+    @Column({ name: 'to_account_id', type: 'int' })
+    fromAccountId!: string;
 
-    @Column({ type: 'timestamp' })
-    date!: Timestamp;
+    @Column({ name: 'to_account_id', type: 'int' })
+    toAccountId!: string;
 
-    @Column({ type: 'varchar', length: '100' })
-    description!: string;
+    @Column({ type: 'varchar', length: '20' })
+    type!: string;
 
-    @Column({ type: 'decimal'  })
+    @Column({ type: 'decimal' })
     value!: number;
 
-    @Column({ type: 'char', length:'1' })
-    type!: Timestamp;
+    @Column({ type: 'timestamp' })
+    date!: Date;
+
+    @Column({ type: 'varchar', length: '255' })
+    description!: string;
     
     @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
-    createdAt!: Date
+    createdAt!: Date;
 
     @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
-    updatedAt!: Date
+    updatedAt!: Date;
     
-}
+};

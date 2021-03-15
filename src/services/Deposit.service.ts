71723adbc3774a023ac6fe50198a991c4e09a71d
rafaleceu <rafa021dev@gmail.com>
2021-03-15 17:46:31 -0300

    feat: Deposit Service

--- src/services/Deposit.service.ts
+++ src/services/Deposit.service.ts
@@ -1,40 +1,24 @@
 import { getRepository } from 'typeorm'
 
-import { AccountsBalance } from '../models/AccountsBalance.model'
-import findAccount from '../services/Accounts.service';
+import BalanceService from './Balance.service'
+import MovementService from './Movement.service';
 
 class DepositService {
 
-    public accountDeposit = async (findAccountId: number, value: number): Promise<any> => {
+    public accountDeposit = async (destinyAccountNumber: number, value: number): Promise<any> => {
 
-        console.log('hi')
-
-        const repository = getRepository(AccountsBalance);
-        let actualMonth = (new Date().getMonth()) + 1;
-        let accountBalanceRegister;
+        let movementType = 'deposit';
+        let newMovementRegister;
+        let newBalanceRegister;
 
         try {
-            accountBalanceRegister = await repository.findOne({month: actualMonth, accountId: findAccountId})
+            newBalanceRegister = await BalanceService.updateActualBalance(destinyAccountNumber, value)
+            newMovementRegister = await MovementService.publishNewMovement(destinyAccountNumber, movementType, value)
         } catch (err) {
             throw err
         }
 
-        if (!accountBalanceRegister?.actualBalance)
-            throw new Error('Balance not found');
-            
-        let newBalance = accountBalanceRegister?.actualBalance + value;
-
-        let updateBalance;
-
-        try {
-            updateBalance = repository.update(accountBalanceRegister.id, {actualBalance: newBalance})
-        } catch (err) {
-            throw err
-        }
-
-        console.log(updateBalance);
-
-
+        return { newBalanceRegister, newMovementRegister };
 
     }
 

a47285eaa3ceacbd3972beba45f4a5a34459f769
rafaleceu <rafa021dev@gmail.com>
2021-03-15 12:41:15 -0300

    feat: add balance and deposit on signup

--- src/services/Signup.service.ts
+++ src/services/Signup.service.ts
@@ -1,8 +1,10 @@
 import UsersService from './Users.service';
 import ClientsService from './Clients.service';
 import AccountsService from './Accounts.service'
+import BalanceService from './Balance.service';
+import DepositService from '../services/Deposit.service';
 
-import mailer from '../services/Mail.service'
+import mailer from '../services/mail.service'
 
 class SingupService {
 
@@ -11,28 +13,24 @@ class SingupService {
         let newUser;
         let newClient;
         let newAccount;
+        let firstBalance;
+        let firstDeposit;
 
         try {
             newUser = await UsersService.newUser(username, password, email, cpf);
-        } catch (err) {
-            throw err
-        };
-
-        try {
             newClient = await ClientsService.newClient(newUser.id, name, adress, phone);
+            newAccount = await AccountsService.newAccount(newUser.id);
+            firstBalance = await BalanceService.firstBalance(newAccount.id);
+            console.log('oi?');
+            firstDeposit = await DepositService.accountDeposit(newAccount.id, 200);
         } catch (err) {
-            throw err
+            throw err;
         };
 
-        try {
-            newAccount = await AccountsService.newAccount(newUser.id);
-        } catch (err) {
-            throw err
-        };
 
         mailer.sendCreateAccountMail(username,newAccount.idBank,newAccount.agency,newAccount.accountNumber)
 
-        return {newUser, newClient, newAccount};
+        return {newUser, newClient, newAccount, firstBalance, firstDeposit};
     }
 
 }

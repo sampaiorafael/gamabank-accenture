872581b177b338714b1402de092777516092134d
rafaleceu <rafa021dev@gmail.com>
2021-03-18 20:33:48 -0300

    refac: server port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Express from './server/Express';
 import configs from './config/configs';
 
-Express.express.listen(configs.Server.port, () => {
+const port = Number(configs.Server.port) || 80; 
+
+Express.express.listen(port, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${configs.Server.port}`);
 });

f541155d1a71b10f67c36cf27b6503d65eebbc75
Juniornbs <njr.mor@gmail.com>
2021-03-20 14:43:27 -0300

    refact: mailer require ssl/tls

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -7,7 +7,7 @@ const setup = async () => {
     const transporter = nodemailer.createTransport({
         host: configs.Mailer.host, 
         port: Number(configs.Mailer.port),
-        secure: false,
+        secure: true,
         auth: {
             user: account ? account.user : configs.Mailer.user,
             pass: account ? account.pass : configs.Mailer.pass
